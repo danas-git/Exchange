4bc7c94b639d734619b68b09f6b4c3e12a833d8a
danas-git <bdhana.blogger@gmail.com>
2015-09-21 15:32:07 +0200

    additem module first commit

--- Exchange/src/com/example/exchange/AddBookOfferFragment.java
+++ Exchange/src/com/example/exchange/AddBookOfferFragment.java
@@ -8,12 +8,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class AddBookOfferFragment extends Fragment {
 	
 	private View fragmentView;
 	private MenuItem menuitem;
-	
+	private ImageButton cameraImageButton;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -33,6 +35,22 @@ public class AddBookOfferFragment extends Fragment {
 		// TODO Auto-generated method stub
 		fragmentView= inflater.inflate(R.layout.add_offer_book_screen, container,false);
 		return fragmentView;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		cameraImageButton = (ImageButton) fragmentView.findViewById(R.id.addbook_imageButtonUpload);
+		cameraImageButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "Camera intent will be added shortly", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
 	}
 	@Override
 	public void onPause() {
